aac8d1a1c9259162f51722f87aea4a8c037726cb
DRMPN <mr.gorden.smit@gmail.com>
2021-09-30 00:01:48 +0300

    testing slash command groups and subgroups

--- CS50/FinalProject/bot_app.py
+++ CS50/FinalProject/bot_app.py
@@ -11,18 +11,31 @@ import os
 bot = lightbulb.Bot(token=os.environ.get("DISCORD_BOT_TOKEN"), prefix="DisBot", logs="DEBUG")
 
 
-# Silly bot command definition 
-class Echo(slash_commands.SlashCommand):
-    description = "Repeats your input."
+class Foo(slash_commands.SlashCommandGroup):
+    description = "Test slash command group."
+
+
+@Foo.subcommand()
+class Bar(slash_commands.SlashSubCommand):
+    description = "Test subcommand."
     # Options
-    text: str = slash_commands.Option("Text to repeat")
+    baz: str = slash_commands.Option("Test subcommand option.")
 
     async def callback(self, context):
-        await context.respond(context.options.text)
+        await context.respond(context.options.baz)
+
+@Foo.subcommand()
+class Baz(slash_commands.SlashSubCommand):
+    description = "Test subcommand 2."
+    # Options
+    bak: str = slash_commands.Option("Test subcommand option 2.")
+
+    async def callback(self, context):
+        await context.respond(context.options.bak)
 
 
 # Add the slash command to the bot
-bot.add_slash_command(Echo)
+bot.add_slash_command(Foo)
 
 
 # Run the bot
