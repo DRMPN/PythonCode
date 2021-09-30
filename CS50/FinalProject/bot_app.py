0ab09141f80535eaaf9db11964e9f959e76b41a5
DRMPN <mr.gorden.smit@gmail.com>
2021-09-30 20:04:42 +0300

    better formatting

--- CS50/FinalProject/bot_app.py
+++ CS50/FinalProject/bot_app.py
@@ -6,35 +6,43 @@ from lightbulb import slash_commands
 import os
 
 
+# Set the logging level
+logging.basicConfig(level=logging.INFO)
+
+
 # Create instance of a bot and set to debug for both lightbulb and hikari
 # TODO: add more parameters
 bot = lightbulb.Bot(token=os.environ.get("DISCORD_BOT_TOKEN"), prefix="DisBot", logs="DEBUG")
 
 
+# Test slash command group
 class Foo(slash_commands.SlashCommandGroup):
     description = "Test slash command group."
 
 
+# Test subcommand #1
 @Foo.subcommand()
 class Bar(slash_commands.SlashSubCommand):
-    description = "Test subcommand."
+    description = "Test subcommand one."
     # Options
-    baz: str = slash_commands.Option("Test subcommand option.")
+    baz: str = slash_commands.Option("Test subcommand option one.")
 
     async def callback(self, context):
         await context.respond(context.options.baz)
 
+
+# Test subcommand #2
 @Foo.subcommand()
 class Baz(slash_commands.SlashSubCommand):
-    description = "Test subcommand 2."
+    description = "Test subcommand two."
     # Options
-    bak: str = slash_commands.Option("Test subcommand option 2.")
+    bak: str = slash_commands.Option("Test subcommand option two.")
 
     async def callback(self, context):
         await context.respond(context.options.bak)
 
 
-# Add the slash command to the bot
+# Add the slash command group to the bot
 bot.add_slash_command(Foo)
 
 
